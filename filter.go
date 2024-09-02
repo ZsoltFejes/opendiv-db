db0933e41c7a2542c62048e7f5d0015415b71620
Zsolt Fejes <zsolt.fejes@icloud.com>
2024-09-02 18:06:14 +0100

    Added feature to skip a filtered block if field isn't defined in the doc

--- filter.go
+++ filter.go
@@ -34,26 +34,26 @@ func (f *Filter) Documents() ([]Document, error) {
 	// the collection is either empty or doesn't exist
 	files, _ := os.ReadDir(dir)
 
+	// Accepted operators
+	operators := map[string]bool{
+		"==": true,
+		"<=": true,
+		">=": true,
+		"!=": true,
+		"<":  true,
+		">":  true}
+
+	// Check to make sure correct condition is provided
+	if _, ok := operators[f.operator]; !ok {
+		return col, fmt.Errorf("Filter '" + f.operator + "' is not supported. Accepted conditions ==, <=, >=, !=, <, > ")
+	}
+
 	// iterate over each of the files, attempting to read the file. If successful
 	// append the files to the collection of read files
 	for _, file := range files {
 		doc, err := f.collection.Document(file.Name())
 		if err != nil {
 			return col, fmt.Errorf("Unable to read file "+file.Name(), false, true)
-		}
-
-		// Accepted operators
-		operators := map[string]bool{
-			"==": true,
-			"<=": true,
-			">=": true,
-			"!=": true,
-			"<":  true,
-			">":  true}
-
-		// Check to make sure correct condition is provided
-		if _, ok := operators[f.operator]; !ok {
-			return col, fmt.Errorf("Filter '" + f.operator + "' is not supported. Accepted conditions ==, <=, >=, !=, <, > ")
 		}
 
 		// Marshal document data into generic map for comparison
@@ -65,38 +65,87 @@ func (f *Filter) Documents() ([]Document, error) {
 		// Find field
 		field := d[f.field]
 		// Check for provided field
-		if field == nil {
-			return col, fmt.Errorf("field not provided")
-		}
+		if field != nil {
 
-		switch real := field.(type) {
-		case string:
-			switch filter_t := f.value.(type) {
+			switch real := field.(type) {
 			case string:
-				// Check if we want to filter based on time
-				document_parsed_time, err := time.Parse(time.RFC3339Nano, real)
-				if err == nil {
-					filter_parsed_time, err := time.Parse(time.RFC3339Nano, filter_t)
-					if err != nil {
-						return col, fmt.Errorf("document filed is RFC3339 formatted time but the filter isn't, unable to parse filter to date time")
+				switch filter_t := f.value.(type) {
+				case string:
+					// Check if we want to filter based on time
+					document_parsed_time, err := time.Parse(time.RFC3339Nano, real)
+					if err == nil {
+						filter_parsed_time, err := time.Parse(time.RFC3339Nano, filter_t)
+						if err != nil {
+							return col, fmt.Errorf("document filed is RFC3339 formatted time but the filter isn't, unable to parse filter to date time")
+						}
+						switch f.operator {
+						case "<":
+							if document_parsed_time.Before(filter_parsed_time) {
+								col = append(col, doc)
+							}
+						case ">":
+							if document_parsed_time.After(filter_parsed_time) {
+								col = append(col, doc)
+							}
+						case "==":
+							if document_parsed_time.Equal(filter_parsed_time) {
+								col = append(col, doc)
+							}
+						default:
+							return col, fmt.Errorf("unsupported operator " + f.operator + " for time")
+						}
+					} else {
+						switch f.operator {
+						case "==":
+							if real == filter_t {
+								col = append(col, doc)
+							}
+						case "!=":
+							if real != filter_t {
+								col = append(col, doc)
+							}
+						default:
+							return col, fmt.Errorf("unsupported operator " + f.operator + " for string")
+						}
 					}
+				default:
+					return col, fmt.Errorf("document field and filter value are mismatched")
+				}
+			case float64:
+				switch filter_t := f.value.(type) {
+				case float64:
 					switch f.operator {
+					case "==":
+						if real == f.value {
+							col = append(col, doc)
+						}
+					case "<=":
+						if real <= filter_t {
+							col = append(col, doc)
+						}
+					case ">=":
+						if real >= filter_t {
+							col = append(col, doc)
+						}
+					case "!=":
+						if real != filter_t {
+							col = append(col, doc)
+						}
 					case "<":
-						if document_parsed_time.Before(filter_parsed_time) {
+						if real < filter_t {
 							col = append(col, doc)
 						}
 					case ">":
-						if document_parsed_time.After(filter_parsed_time) {
+						if real > filter_t {
 							col = append(col, doc)
 						}
-					case "==":
-						if document_parsed_time.Equal(filter_parsed_time) {
-							col = append(col, doc)
-						}
-					default:
-						return col, fmt.Errorf("unsupported operator " + f.operator + " for time")
 					}
-				} else {
+				default:
+					return col, fmt.Errorf("Filter Value is not float64. For more details: https://pkg.go.dev/encoding/json#Unmarshal")
+				}
+			case bool:
+				switch filter_t := f.value.(type) {
+				case bool:
 					switch f.operator {
 					case "==":
 						if real == filter_t {
@@ -107,61 +156,11 @@ func (f *Filter) Documents() ([]Document, error) {
 							col = append(col, doc)
 						}
 					default:
-						return col, fmt.Errorf("unsupported operator " + f.operator + " for string")
-					}
-				}
-			default:
-				return col, fmt.Errorf("document field and filter value are mismatched")
-			}
-		case float64:
-			switch filter_t := f.value.(type) {
-			case float64:
-				switch f.operator {
-				case "==":
-					if real == f.value {
-						col = append(col, doc)
-					}
-				case "<=":
-					if real <= filter_t {
-						col = append(col, doc)
-					}
-				case ">=":
-					if real >= filter_t {
-						col = append(col, doc)
-					}
-				case "!=":
-					if real != filter_t {
-						col = append(col, doc)
-					}
-				case "<":
-					if real < filter_t {
-						col = append(col, doc)
-					}
-				case ">":
-					if real > filter_t {
-						col = append(col, doc)
-					}
-				}
-			default:
-				return col, fmt.Errorf("Filter Value is not float64. For more details: https://pkg.go.dev/encoding/json#Unmarshal")
-			}
-		case bool:
-			switch filter_t := f.value.(type) {
-			case bool:
-				switch f.operator {
-				case "==":
-					if real == filter_t {
-						col = append(col, doc)
-					}
-				case "!=":
-					if real != filter_t {
-						col = append(col, doc)
+						return col, fmt.Errorf("unsupported operator " + f.operator + " for bool")
 					}
 				default:
-					return col, fmt.Errorf("unsupported operator " + f.operator + " for bool")
+					return col, fmt.Errorf("document field and filter value are mismatched")
 				}
-			default:
-				return col, fmt.Errorf("document field and filter value are mismatched")
 			}
 		}
 	}
